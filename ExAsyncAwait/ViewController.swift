651885c4dcf800e1721d917b03ab89d105b94767
jake <palatable7@naver.com>
2023-09-30 15:25:43 +0900

    Implement

--- ExAsyncAwait/ViewController.swift
+++ ExAsyncAwait/ViewController.swift
@@ -14,9 +14,26 @@ class ViewController: UIViewController {
         
         let imageUrlString = "https://images.dog.ceo/breeds/australian-shepherd/pepper.jpg"
         guard let url = URL(string: imageUrlString) else { return }
-//        Task {
-            let image = try await loadImage(url: url)
-//        }
+        Task {
+            let image1 = try await loadImage(url: url)
+            // wait for loading image...
+            let image2 = try await loadImage(url: url)
+            let image3 = try await loadImage(url: url)
+            let image4 = try await loadImage(url: url)
+            
+            try await imagesForConcurrency()
+        }
+    }
+    
+    func imagesForConcurrency() async throws -> [UIImage] {
+        let imageUrlString = "https://images.dog.ceo/breeds/australian-shepherd/pepper.jpg"
+        guard let url = URL(string: imageUrlString) else { return [] }
+        
+        async let first = loadImage(url: url)
+        // no wait for loading image
+        async let second = loadImage(url: url)
+        async let third = loadImage(url: url)
+        return try await [first, second, third]
     }
 }
 
